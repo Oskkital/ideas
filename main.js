a595ef6e39849d30ae0e90b9501fcbad4c40c197
Álvaro Aparicio <63745502+alvaro2203@users.noreply.github.com>
2024-01-26 11:35:05 +0100

    code refactorized

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const ideaList = document.querySelector("#idea-list");
+const ideasContainer = document.querySelector("#ideas-container");
 const newIdeaInput = document.querySelector("#new-idea-input");
 const newIdeaButton = document.querySelector("#add-idea-button");
 
@@ -6,15 +6,21 @@ const ideas = [];
 
 const app = {
   ideas: ideas,
-  ideasList: ideaList,
+  ideasContainer: ideasContainer,
   newIdeaInput: newIdeaInput,
 };
 
-function saveIdeasToLocalStorage(ideas) {
-  localStorage.setItem("ideas", JSON.stringify(ideas));
+// Returns the idea object {id, title, isRead}
+function createObjectIdea(title, isRead = false) {
+  return {
+    id: Date.now(),
+    title,
+    isRead,
+  };
 }
 
-function editIdea(idea) {
+// Returns the updated idea object {id, title, isRead} with the new title
+function editObjectIdea(idea) {
   const newTitle = prompt("Editar idea:", idea.title);
   if (newTitle != "") {
     idea.title = newTitle;
@@ -23,6 +29,95 @@ function editIdea(idea) {
   }
 }
 
+// Adds the given idea to the ideas-container div
+function addIdeaToContainer(idea, ideasContainer) {
+  const ideaElement = renderCardIdea(idea);
+  ideasContainer.appendChild(ideaElement);
+}
+
+// Handles the add-idea-button taking the new-idea-input
+function handleAddIdeaButton(app) {
+  const newIdeaTitle = app.newIdeaInput.value;
+
+  if (newIdeaTitle != "") {
+    const newIdea = createObjectIdea(newIdeaTitle);
+    app.ideas.push(newIdea);
+
+    addIdeaToContainer(newIdea, app.ideasContainer);
+    saveIdeasToLocalStorage(app.ideas);
+    app.newIdeaInput.value = "";
+
+    console.log("Nueva idea añadida:", newIdea);
+  }
+}
+
+// Returns the card-idea element required for the ideas-container
+function renderCardIdea(idea) {
+  const ideaElement = document.createElement("div");
+  ideaElement.setAttribute("id", "card-idea");
+
+  const ideaCheckbox = document.createElement("input");
+  ideaCheckbox.type = "checkbox";
+  ideaCheckbox.checked = idea.isRead;
+
+  const ideaText = document.createElement("span");
+  ideaText.className = "text";
+  ideaText.textContent = idea.title;
+  ideaText.classList.toggle("completed", idea.isRead);
+
+  ideaCheckbox.addEventListener("change", () => {
+    idea.isRead = ideaCheckbox.checked;
+    ideaText.classList.toggle("completed", idea.isRead);
+    saveIdeasToLocalStorage(app.ideas);
+  });
+
+  const ideaEditButton = document.createElement("button");
+  ideaEditButton.textContent = "Editar";
+  ideaEditButton.className = "edit-button";
+  ideaEditButton.addEventListener("click", () => editObjectIdea(idea));
+
+  const ideaDeleteButton = document.createElement("button");
+  ideaDeleteButton.textContent = "Eliminar";
+  ideaDeleteButton.className = "delete-button";
+  ideaDeleteButton.addEventListener("click", () => {
+    ideaElement.remove();
+    const ideaIndex = app.ideas.indexOf(idea);
+
+    if (ideaIndex > -1) {
+      app.ideas.splice(ideaIndex, 1);
+    }
+
+    saveIdeasToLocalStorage(app.ideas);
+  });
+
+  const ideaDate = document.createElement("span");
+  ideaDate.className = "date";
+
+  const currentDate = new Date();
+
+  const formattedDate = `${currentDate.getDate()}/${
+    currentDate.getMonth() + 1
+  }/${currentDate.getFullYear()} ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`;
+
+  ideaDate.textContent = formattedDate;
+
+  ideaElement.appendChild(ideaDate);
+  ideaElement.appendChild(ideaText);
+  ideaElement.appendChild(ideaEditButton);
+  ideaElement.appendChild(ideaDeleteButton);
+  ideaElement.appendChild(ideaCheckbox);
+
+  ideaElement.setAttribute("data-idea-id", idea.id);
+
+  return ideaElement;
+}
+
+
+function saveIdeasToLocalStorage(ideas) {
+  localStorage.setItem("ideas", JSON.stringify(ideas));
+}
+
+
 function updateIdeaElement(idea) {
   const ideaElement = document.querySelector(`[data-idea-id="${idea.id}"]`);
   const ideaText = ideaElement.querySelector("span");
@@ -30,110 +125,14 @@ function updateIdeaElement(idea) {
 }
 
 window.onload = () => {
+  // Get the list stored in Local Storage to save it in app.ideas
   const savedIdeas = JSON.parse(localStorage.getItem("ideas")) || [];
-  app.ideas = savedIdeas.map((idea) => {
-    return createIdea(idea.title, idea.isRead);
-  });
-  app.ideas.forEach((idea) => {
-    return addIdeaToList(idea, app.ideasList);
-  });
+  app.ideas = savedIdeas.map((idea) => createObjectIdea(idea.title, idea.isRead));
 
-  function createIdea(title, isRead = false) {
-    return {
-      id: Date.now(),
-      title,
-      isRead,
-    };
-  }
+  // Add each idea from app.ideas to ideas-container
+  app.ideas.forEach((idea) => addIdeaToContainer(idea, app.ideasContainer));
 
-  function addIdeaToList(idea, ideaList) {
-    const ideaElement = renderTable(idea);
-    ideaList.appendChild(ideaElement);
-  }
-
-  function addIdea(app) {
-    const newIdeaTitle = app.newIdeaInput.value;
-    console.log(typeof newIdeaTitle);
-    if (newIdeaTitle != "") {
-      const newIdea = createIdea(newIdeaTitle);
-      app.ideas.push(newIdea);
-
-      addIdeaToList(newIdea, app.ideasList);
-      saveIdeasToLocalStorage(app.ideas);
-      app.newIdeaInput.value = "";
-
-      console.log("Nueva idea añadida:", newIdea);
-    }
-  }
-
-  function renderTable(idea) {
-    const ideaElement = document.createElement("div");
-    ideaElement.setAttribute("id", "card-idea");
-
-    const ideaCheckbox = document.createElement("input");
-    ideaCheckbox.type = "checkbox";
-    ideaCheckbox.checked = idea.isRead;
-
-    const ideaText = document.createElement("span");
-    ideaText.className = "text";
-    ideaText.textContent = idea.title;
-    ideaText.classList.toggle("completed", idea.isRead);
-
-    ideaCheckbox.addEventListener("change", () => {
-      idea.isRead = ideaCheckbox.checked;
-      ideaText.classList.toggle("completed", idea.isRead);
-      saveIdeasToLocalStorage(app.ideas);
-    });
-
-    const ideaEditButton = document.createElement("button");
-    ideaEditButton.textContent = "Editar";
-    ideaEditButton.className = "edit-button";
-    ideaEditButton.addEventListener("click", () => {
-      editIdea(idea);
-    });
-
-    const ideaDeleteButton = document.createElement("button");
-    ideaDeleteButton.textContent = "Eliminar";
-    ideaDeleteButton.className = "delete-button";
-    ideaDeleteButton.addEventListener("click", () => {
-      ideaElement.remove();
-      const ideaIndex = app.ideas.indexOf(idea);
-
-      if (ideaIndex > -1) {
-        app.ideas.splice(ideaIndex, 1);
-      }
-      saveIdeasToLocalStorage(app.ideas);
-    });
-
-    const ideaDate = document.createElement("span");
-    ideaDate.className = "date";
-
-    const currentDate = new Date();
-
-    const formattedDate = `${currentDate.getDate()}/${
-      currentDate.getMonth() + 1
-    }/${currentDate.getFullYear()} ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`;
-
-    ideaDate.textContent = formattedDate;
-
-    ideaElement.appendChild(ideaDate);
-    ideaElement.appendChild(ideaText);
-    ideaElement.appendChild(ideaEditButton);
-    ideaElement.appendChild(ideaDeleteButton);
-    ideaElement.appendChild(ideaCheckbox);
-
-    ideaElement.setAttribute("data-idea-id", idea.id);
-
-    return ideaElement;
-  }
-
-  newIdeaButton.addEventListener("click", () => {
-    addIdea(app);
-  });
-
-  newIdeaInput.addEventListener("keydown", (event) => {
-    if (event.key === "Enter") {
-      addIdea(app);
-    }
-  });
+  // Required events
+  newIdeaButton.addEventListener("click", () => handleAddIdeaButton(app));
+  newIdeaInput.addEventListener("keydown", (event) => (event.key === "Enter") && handleAddIdeaButton(app));
 };
